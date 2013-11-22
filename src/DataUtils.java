622d55c2322ea4103aacff48b5b719afb88a9150
Alex Wolff <A.Leon.Wolff@gmail.com>
2013-11-22 14:03:26 -0500

    utils file

---
+++ src/DataUtils.java
@@ -0,0 +1,18 @@
+
+public class DataUtils {
+	//number of directions for each sensor (X,Y,Z)
+    public static final int NUM_DIRECTIONS = 3;
+    
+    //approach event
+    public static final String APPROACH_EVENT = "approach";
+  //enter event
+    public static final String ENTER_EVENT = "enter";
+  //ignition on event
+    public static final String ON_EVENT = "ignition on";
+  //ignition off event
+    public static final String OFF_EVENT = "ignition off";
+  //exit event
+    public static final String EXIT_EVENT = "exit";
+  //no event
+    public static final String NO_EVENT = "none";
+}
